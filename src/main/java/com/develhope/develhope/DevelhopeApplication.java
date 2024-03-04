e1f990f72a6a501c4b5e09fa5fbbc6265719e35b
Rykard <rickygalante@hotmail.it>
2024-03-04 18:56:02 +0100

    aggiornamento

---
+++ src/main/java/com/develhope/develhope/DevelhopeApplication.java
@@ -0,0 +1,13 @@
+package com.develhope.develhope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelhopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelhopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/develhope/develhope/DevelhopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develhope.develhope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelhopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
